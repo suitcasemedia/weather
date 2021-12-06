46a4cbb6a8abb7a7425d83bd010ebb895cc71ec2
jimmy <jimmyjconnelly@gmail.com>
2021-12-06 12:39:34 +0000

    update dependency in react hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector} from 'react-redux';
 import {
   getPendingSelector,
   getWeatherSelector,
@@ -18,8 +18,8 @@ const App = () => {
   const [city, updateCity] = useState('London');
 
   useEffect(() => {
-    dispatch(fetchWeatherRequest('london'));
-  }, []);
+    dispatch(fetchWeatherRequest('London'));
+  }, [dispatch]);
 
   const handleChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     updateCity(e.currentTarget.value);
